e0bd95bb3d435c185d97f87a62b50c0e321c2a81
tam-artem <tam-artem@mail.ru>
2022-12-15 17:50:38 +0300

    15.12.2022

---
+++ 12/10388.py
@@ -0,0 +1,10 @@
+s = '5' * 54 + '7'
+while '722' in s or '557' in s:
+    if '722' in s:/
+        s = s.replace('722', '57', 1)
+    elif '557' in s:
+        s = s.replace('557', '72', 1)
+print(s)
+
+
+
---
+++ 2/47206.py
@@ -0,0 +1,10 @@
+print("x", "y", "z", "w")
+for x in range(0,2):
+    for y in range(0,2):
+        for z in range(0,2):
+            for w in range(0,2):
+                if(not ((y <= x)) or (z <= w) or not (z)) == 0:
+                    print(x, y, z, w)
+
+
+
