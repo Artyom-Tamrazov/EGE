8ed37d07d3c6652248d1fa7a203ac7ddefd1518d
Андрей Боев <a.boev.it@gmail.com>
2023-01-26 19:30:13 +0300

    Добавлен номер 5

---
+++ 5/46963.py
@@ -0,0 +1,13 @@
+for n in range(10000):
+    # Перевод n в двоичную систему
+    b = bin(n)[2:]
+    sum_0 = 0
+    sum_1 = 0
+    for i in range(len(b)):
+        if i % 2 != 0 and b[i] == '1':
+            sum_1 += 1
+        elif i % 2 == 0 and b[i] == '0':
+            sum_0 += 1
+    if abs(sum_1 - sum_0) == 5:
+        print(n)
+        break
